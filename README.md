b91cd3218377d8addf98c0e3f0e97f071c685141
= <aldhiego666@gmail.com>
2023-07-09 21:28:41 -0300

    initial

--- README.md
+++
@@ -1,1 +0,0 @@
-# pet-shop-system
---
+++ frondend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 0,
+    'import/extensions': 0,
+  },
+}
