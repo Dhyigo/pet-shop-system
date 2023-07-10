5d67546435e8b5eaff5e12d1c0df6ecc89e42a98
= <aldhiego666@gmail.com>
2023-07-10 16:20:01 -0300

    ProtectdRoutes: buscando autenticação

--- frontend/src/router/ProtectedRoutes.tsx
+++ frontend/src/router/ProtectedRoutes.tsx
@@ -1,7 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import { authStore } from '../store';
 
 export function ProtectedRoutes() {
-  const auth = true;
+  const auth = authStore.get();
 
   return auth ? <Outlet /> : <Navigate to="/login" />;
 }
