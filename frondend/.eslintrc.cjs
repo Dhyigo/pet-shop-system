b6cd84d51932062f358827ea629df8c4bb41e169
= <aldhiego666@gmail.com>
2023-07-10 00:45:22 -0300

    page: login

--- frondend/.eslintrc.cjs
+++ frondend/.eslintrc.cjs
@@ -21,5 +21,6 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'import/extensions': 0,
     'import/prefer-default-export': 0,
+    'react/jsx-props-no-spreading': 0,
   },
 };
--- frondend/src/router/index.tsx
+++ frondend/src/router/index.tsx
@@ -1,8 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Login } from '../page/Login';
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/home" element={<h1>Hello, world!</h1>} />
       <Route path="/*" element={<Navigate to="/home" />} />
     </Routes>
