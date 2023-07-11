34c4d0f875ffa4ddf1648a2c067abb9fce4e8464
= <aldhiego666@gmail.com>
2023-07-11 20:43:07 -0300

    login: refatorando

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import { z } from 'zod';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import './Login.css';
 import { useNavigate } from 'react-router-dom';
 import { userServices, ApiException } from '../../services';
+import { Form } from '../../components';
 
 const loginSchema = z.object({
   email: z.string().email('Coloque um email válido'),
@@ -15,11 +15,14 @@ type LoginData = z.infer<typeof loginSchema>;
 
 export function Login() {
   const navigate = useNavigate();
-  const { register, handleSubmit, formState } = useForm<LoginData>({
+  const loginUserForm = useForm<LoginData>({
     resolver: zodResolver(loginSchema),
   });
 
-  const { errors } = formState;
+  const {
+    handleSubmit,
+    formState: { isLoading },
+  } = loginUserForm;
 
   const loginUser = async (data: LoginData) => {
     const { email, password } = data;
@@ -35,57 +38,28 @@ export function Login() {
 
   return (
     <div className="card shadow rounded">
-      <div className="card-header bg-success text-white text-center">
+      <h1 className="card-header bg-success h6 fw-normal lh-base text-white text-center">
         Entre com sua conta
-      </div>
-      <div className="card-body">
-        <form onSubmit={handleSubmit(loginUser)}>
-          <div className="form-row">
-            <div className="col-12">
-              <label htmlFor="email">Email</label>
-              <input
-                className="form-control"
-                {...register('email')}
-                id="email"
-                type="email"
-                placeholder="Digite seu email"
-              />
+      </h1>
+      <FormProvider {...loginUserForm}>
+        <Form.Root onSubmit={handleSubmit(loginUser)}>
+          <Form.Field>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Input name="email" placeholder="Digite seu email" />
+            <Form.ErrorMessage field="email" />
+          </Form.Field>
 
-              {errors.email?.message && <span>{errors.email.message}</span>}
-            </div>
-          </div>
-          <div className="form-row mt-3">
-            <div className="col-12">
-              <label htmlFor="password">Senha</label>
-              <input
-                className="form-control"
-                {...register('password')}
-                id="password"
-                type="password"
-                placeholder="Digite sua senha"
-              />
+          <Form.Field>
+            <Form.Label htmlFor="password">Senha</Form.Label>
+            <Form.Input name="password" placeholder="Digite sua senha" />
+            <Form.ErrorMessage field="password" />
+          </Form.Field>
 
-              {errors.password?.message && (
-                <span>{errors.password.message}</span>
-              )}
-            </div>
-          </div>
-          <div className="form-row mt-3">
-            <div className="col-12">
-              <button type="submit" className="btn btn-success">
-                Entrar
-              </button>
-            </div>
-          </div>
-          <div className="form-row mt-3">
-            <div className="col-12 text-center">
-              <span className="notRegisteredText text-primary">
-                Não possui uma conta?
-              </span>
-            </div>
-          </div>
-        </form>
-      </div>
+          <Form.Button isLoading={isLoading} text="Entrar" />
+
+          <Form.Link to="/criar-conta" text="Não possui uma conta?" />
+        </Form.Root>
+      </FormProvider>
     </div>
   );
 }
