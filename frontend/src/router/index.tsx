7859c6da06758d6f86e3e40068d2a1550939e7de
= <aldhiego666@gmail.com>
2023-07-12 18:00:13 -0300

    router: rota da página home

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,15 +1,16 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Login } from '../pages/login/Login';
 import { ProtectedRoutes } from './ProtectedRoutes';
+import { Home } from '../pages/home/Home';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route element={<ProtectedRoutes />}>
-        <Route path="/home" element={<h1>Hello, world!</h1>} />
+        <Route path="/" element={<Home />} />
       </Route>
-      <Route path="/*" element={<Navigate to="/home" />} />
+      <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
