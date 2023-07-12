374baccbc655f28bdaf2898c5d46ff1f5279e816
= <aldhiego666@gmail.com>
2023-07-11 23:30:32 -0300

    form-root: remoção da class

--- frontend/src/components/Form/FormRoot.tsx
+++ frontend/src/components/Form/FormRoot.tsx
@@ -1,5 +1,5 @@
 import { FormHTMLAttributes } from 'react';
 
 export function FormRoot(props: FormHTMLAttributes<HTMLFormElement>) {
-  return <form className="card-body" {...props} />;
+  return <form {...props} />;
 }
