ae1f55dfa126c3d6c0589f27fc6693647fa88d9c
= <aldhiego666@gmail.com>
2023-07-13 01:47:22 -0300

    NavBarMenu: colocando padding right

--- frontend/src/components/NavBar/NavBarMenu.tsx
+++ frontend/src/components/NavBar/NavBarMenu.tsx
@@ -3,7 +3,7 @@ import { HTMLAttributes } from 'react';
 export function NavBarMenu(props: HTMLAttributes<HTMLUListElement>) {
   return (
     <ul
-      className="navbar-nav align-items-lg-center justify-content-start flex-grow-1 pe-4"
+      className="navbar-nav align-items-lg-center justify-content-start flex-grow-1 ps-1"
       {...props}
     />
   );
