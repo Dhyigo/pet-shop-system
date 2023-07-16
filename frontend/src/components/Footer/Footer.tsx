b151f5e9bb7bd2a0a918bd0da1a851babcff3ce7
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-07-16 19:37:53 -0300

    Footer: Adiciona componente footer.

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import { BsFacebook } from 'react-icons/bs';
+
+export function Footer() {
+  return (
+    <footer>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#198754"
+          fill-opacity="1"
+          d="M0,96L30,106.7C60,117,120,139,180,133.3C240,128,300,96,360,106.7C420,117,480,171,540,192C600,213,660,203,720,170.7C780,139,840,85,900,80C960,75,1020,117,1080,160C1140,203,1200,245,1260,234.7C1320,224,1380,160,1410,128L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
+        ></path>
+        <text x="1%" y="95%" font-family="Verdana" font-size="10" fill="white">
+          &#169; 2023 PetShop-Sytem, TechnologyFriends, Brasil. Todos os
+          direitos reservados.
+        </text>
+        <text x="80%" y="95%" font-family="Verdana" font-size="9" fill="white">
+          Adriel Santos, Diego, Ryan Menezes
+        </text>
+      </svg>
+    </footer>
+  );
+}
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Form';
 export * from './Header/Header';
+export * from './Footer/Footer';
