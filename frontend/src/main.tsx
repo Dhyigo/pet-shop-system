97a269c7dc36939df20b2abb26e8ac6f3fd22e5d
= <aldhiego666@gmail.com>
2023-07-19 17:53:35 -0300

    instalando react-bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 import { App } from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App /> 
+    <App />
   </React.StrictMode>
 );
