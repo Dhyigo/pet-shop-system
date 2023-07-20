0990bb961cd38c44d3a494cc2883568533f90290
= <aldhiego666@gmail.com>
2023-07-20 17:40:52 -0300

    form: add select no form

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -5,11 +5,13 @@ import { FormField } from './FormField';
 import { FormButton } from './FormButton';
 import { FormErrorMessage } from './FormErrorMessage';
 import { FormLink } from './FormLink';
+import { FormSelect } from './FormSelect';
 
 export const Form = {
   Root: FormRoot,
   Field: FormField,
   Label: FormLabel,
+  Select: FormSelect,
   Input: FormInput,
   Button: FormButton,
   ErrorMessage: FormErrorMessage,
