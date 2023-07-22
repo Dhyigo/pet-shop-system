fb312ef9152756e44739353ab2f61545ec8193ef
Adriel dos Santos <adrieldossantos7@gmail.com>
2023-07-21 21:03:13 -0300

    NavBar: Adiciona chamada de logOut() e redirecionamento para /login

--- frontend/src/components/NavBar/NavBarUser.tsx
+++ frontend/src/components/NavBar/NavBarUser.tsx
@@ -1,8 +1,11 @@
 import { Dropdown } from 'react-bootstrap';
 import { NavBarUserProps } from '../../models/components/Navbar';
 import { BiSolidUserCircle } from 'react-icons/bi';
+import { userServices } from '../../services';
+import { useNavigate } from 'react-router-dom';
 
 export function NavBarUser(props: NavBarUserProps) {
+  const navigate = useNavigate();
   return (
     <Dropdown>
       <Dropdown.Toggle variant="light" id="dropdown-basic">
@@ -12,7 +15,15 @@ export function NavBarUser(props: NavBarUserProps) {
 
       <Dropdown.Menu>
         <Dropdown.Item href="#/action-1">Configs</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Sair</Dropdown.Item>
+        <Dropdown.Item
+          href="#/action-2"
+          onClick={() => {
+            userServices.logOut();
+            navigate('/login');
+          }}
+        >
+          Sair
+        </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
