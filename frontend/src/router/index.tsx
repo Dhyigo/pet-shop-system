ed21194fcf8074268ac79dbecbec28d8f2459927
= <aldhiego666@gmail.com>
2023-07-31 22:51:45 -0300

    router: rota de criar conta

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -2,11 +2,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Login } from '../pages/login/Login';
 import { ProtectedRoutes } from './ProtectedRoutes';
 import { Home } from '../pages/home/Home';
+import { Register } from '../pages/register/Register';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/criar-conta" element={<Register />} />
       <Route element={<ProtectedRoutes />}>
         <Route path="/" element={<Home />} />
       </Route>
