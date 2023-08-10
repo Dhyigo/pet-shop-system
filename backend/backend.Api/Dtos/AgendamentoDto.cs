e0b5648e245c869376ab280c3f9efc46b6134241
= <aldhiego666@gmail.com>
2023-08-10 18:28:47 -0300

    agendamento-dto: criação do dto de agendamento

---
+++ backend/backend.Api/Dtos/AgendamentoDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace backend.Api.Dtos
+{
+    public class AgendamentoDto
+    {
+        [Required]
+        public int CodCliente { get; set; }
+
+        [Required]
+        public int CodEmpresa { get; set; }
+
+        public double Total { get; set; }
+
+        [Required]
+        public DateTime DataInicio { get; set; } = new DateTime();
+
+        [Required]
+        public DateTime DataFim { get; set; } = new DateTime();
+
+    }
+}
